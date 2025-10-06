0fc7cb0860d1693cba2c534418d02e1b37a33c0f
donendosted <debasmitbos22@gmail.com>
2025-10-06 13:30:50 +0530

    add title gif

--- src/main.tsx
+++ src/main.tsx
@@ -15,13 +15,14 @@ function Root() {
   if (!started) {
     return (
       <div className="h-screen w-screen bg-black flex flex-col gap-3 items-center justify-center text-white">
-          <h1 className="text-2xl">A lovely game by dos</h1>
-          <button
-            onClick={enterFullscreen}
-            className="px-6 py-3 bg-blue-600 rounded-xl hover:bg-blue-700"
-          >
-            Start Game
-          </button>
+        <img src="/title.gif" className="w-60 sm:w-40"/>
+        <h1 className="text-2xl">A lovely game by dos</h1>
+        <button
+          onClick={enterFullscreen}
+          className="px-6 py-3 bg-blue-600 rounded-xl hover:bg-blue-700"
+        >
+          Start Game
+        </button>
       </div>
     );
   }
